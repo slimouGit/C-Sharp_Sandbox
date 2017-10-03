1bef06d40f727746ce68b7066ed5a774e81dbe48
Salim Oussayfi <box@slimou.net>
2017-10-03 13:16:52 +0200

    ...

--- Decisions/Decisions/Program.cs
+++ Decisions/Decisions/Program.cs
@@ -12,19 +12,21 @@ namespace Decisions
         {
             string eventName = "";
             eventName = Console.ReadLine();
-        
-            if(eventName == "Kino"){
-            Console.WriteLine("hebe 25 Euro ab");
+
+            if (eventName == "Kino")
+            {
+                Console.WriteLine("hebe 25 Euro ab");
             }
             if (eventName == "Konzert")
             {
                 Console.WriteLine("hebe 80 Euro ab");
             }
-            else {
+            if (eventName == "")
+            {
                 Console.WriteLine("hebe 10 Euro ab");
             }
 
-
+            Console.Read();
         }
     }
 }
