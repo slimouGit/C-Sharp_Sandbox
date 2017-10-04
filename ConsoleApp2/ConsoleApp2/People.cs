e0396a47fb39682e817f91f712932e28092cf643
Salim Oussayfi <box@slimou.net>
2017-10-04 08:18:42 +0200

    ...

---
+++ ConsoleApp2/ConsoleApp2/People.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstClass
+{
+    class People
+    {
+        private int age;
+        private string forename;
+        private string surname;
+        private string eyecolor;
+
+        public People(int age, string forename, string surname, string eyecolor)
+        {
+            this.age = age;
+            this.forename = forename;
+            this.surname = surname;
+            this.eyecolor = eyecolor;
+        }
+
+        public People(int age, string forename, string surname)
+        {
+            this.age = age;
+            this.forename = forename;
+            this.surname = surname;
+        }
+
+        public void greetingPerson() {
+            if (this.eyecolor != null)
+            {
+                Console.WriteLine("Vorname " + this.forename + ", Nachname " + this.surname + ", Alter: " + this.age + " Jahre, " + "Augenfarbe: " + this.eyecolor);
+            }
+            else {
+                Console.WriteLine("Vorname " + this.forename + ", Nachname " + this.surname + ", Alter: " + this.age + " Jahre");
+            }
+        }
+
+        public void setAge(int newAge) {
+            if (newAge > this.age) {
+                newAge = this.age;
+                Console.WriteLine("Das Alter von " + this.forename + " wurde nicht geändert");
+            }
+            this.age = newAge;
+        }
+
+        public string getEyeColor() {
+            return this.eyecolor;
+        }
+
+        public string getAge() {
+            return this.age;
+        }
+
+
+
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            People mimi = new People(41, "Mimi", "Illmi", "blau");
+            mimi.setAge(44);
+
+            mimi.greetingPerson();
+
+            People max = new People(28, "Max", "Mustermann");
+            max.greetingPerson();
+
+            People salim = new People(37, "Salim", "Oussayfi", "braun");
+            salim.greetingPerson();
+
+            
+
+            Console.WriteLine(mimi.getEyeColor());
+
+            Console.ReadLine();
+        }
+    }
+}
