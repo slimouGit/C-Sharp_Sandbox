7eb4b4dde96da67a0ae85379db588baa4486b691
slimouGit <box@slimou.net>
2017-10-05 17:24:21 +0200

    Add files via upload

---
+++ Exceptions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exceptions
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Wie alt bist Du");
+
+			string input = Console.ReadLine();
+
+			try
+			{
+				int age = Int32.Parse(input);
+				Console.WriteLine("Du bist " + input + " Jahre alt");
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(input + " ist keine Zahl – " + e.Message);
+			}
+			finally {
+				Console.WriteLine("Programm beendet");
+			}
+
+
+			Console.Read();
+		}
+	}
+}
