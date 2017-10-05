ace53df0dbcd31bc9130d43cb5b1f023d616f636
Salim Oussayfi <box@slimou.net>
2017-10-05 07:25:26 +0200

    ...

---
+++ ArrayList_Session/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayList_Session
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList students = new ArrayList();
+            students.Add("Hans");
+            students.Add("Petra");
+            students.Add("Meike");
+            students.Add("Paul");
+
+            students.Remove("Hans");
+            students.RemoveAt(1);
+
+            Console.WriteLine(students.Count);
+
+            Console.Read();
+
+        }
+    }
+}
